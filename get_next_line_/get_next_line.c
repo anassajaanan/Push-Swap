d9c8f3a79aef7023ea4d8bb671471bf873d59568
Anas Ajaanan <aajaanan@lab1r5s4.42abudhabi.ae>
2023-07-18 08:56:02 +0400

    remove .git from get_next_line

---
+++ get_next_line_/get_next_line.c
@@ -0,0 +1,125 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aajaanan <aajaanan@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/04 14:16:04 by aajaanan          #+#    #+#             */
+/*   Updated: 2023/07/18 08:53:49 by aajaanan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/get_next_line.h"
+#include <stdlib.h>
+
+int	contains_newline(t_queue *q)
+{
+	struct s_queue_node	*tmp;
+
+	tmp = q->front;
+	while (tmp)
+	{
+		if (tmp->val == '\n')
+			return (1);
+		tmp = tmp->next;
+	}
+	return (0);
+}
+
+char	*get_line(t_queue *q)
+{
+	int					i;
+	char				*line;
+	struct s_queue_node	*tmp;
+
+	i = 0;
+	tmp = q->front;
+	while (tmp && tmp->val != '\n')
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	line = (char *)malloc(sizeof(char) * (i + 2));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (q->front && q->front->val != '\n')
+		line[i++] = dequeue(q);
+	line[i++] = dequeue(q);
+	line[i] = '\0';
+	return (line);
+}
+
+char	*get_last_line(t_queue *q)
+{
+	int					i;
+	char				*line;
+	struct s_queue_node	*tmp;
+
+	i = 0;
+	tmp = q->front;
+	while (tmp)
+	{
+		i++;
+		tmp = tmp->next;
+	}
+	line = (char *)malloc(sizeof(char) * (i + 1));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (q->front)
+		line[i++] = dequeue(q);
+	line[i] = '\0';
+	return (line);
+}
+
+char	*handle_bytes(ssize_t bytes, t_queue *q, char *buffer, int *fd)
+{
+	ssize_t	i;
+
+	if (bytes == -1)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	else if (bytes == 0)
+	{
+		free(buffer);
+		if (queue_is_empty(q))
+			return (NULL);
+		else
+			return (get_last_line(q));
+	}
+	else
+	{
+		i = 0;
+		while (i < bytes)
+			enqueue(q, buffer[i++]);
+		free(buffer);
+		return (get_next_line(*fd));
+	}
+}
+
+char	*get_next_line(int fd)
+{
+	static t_queue	q;
+	ssize_t			bytes;
+	char			*buffer;
+	static int		flag = 0;
+
+	if (fd == -1 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (flag == 0)
+	{
+		init_queue(&q);
+		flag = 1;
+	}
+	if (!queue_is_empty(&q) && contains_newline(&q))
+		return (get_line(&q));
+	buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE);
+	if (!buffer)
+		return (NULL);
+	bytes = read(fd, buffer, BUFFER_SIZE);
+	return (handle_bytes(bytes, &q, buffer, &fd));
+}
---
+++ get_next_line_/get_next_line_utils.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aajaanan <aajaanan@student.42abudhabi.a    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/04 14:56:35 by aajaanan          #+#    #+#             */
+/*   Updated: 2023/07/04 19:45:51 by aajaanan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/get_next_line.h"
+
+void	init_queue(t_queue *q)
+{
+	q->front = NULL;
+	q->rear = NULL;
+}
+
+void	enqueue(t_queue *q, char c)
+{
+	struct s_queue_node	*new_node;
+
+	new_node = (struct s_queue_node *)malloc(sizeof(struct s_queue_node));
+	new_node->val = c;
+	new_node->next = NULL;
+	if (q->front == NULL)
+	{
+		q->front = new_node;
+		q->rear = new_node;
+	}
+	else
+	{
+		q->rear->next = new_node;
+		q->rear = new_node;
+	}
+}
+
+char	dequeue(t_queue *q)
+{
+	char				value;
+	struct s_queue_node	*tmp;
+
+	tmp = q->front;
+	q->front = q->front->next;
+	value = tmp->val;
+	free(tmp);
+	return (value);
+}
+
+int	queue_is_empty(t_queue *q)
+{
+	if (q->front == NULL)
+		return (1);
+	return (0);
+}
