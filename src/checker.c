a73b52fba246c8a305cd6f0b595bbda78f8cb3c0
Anas Ajaanan <aajaanan@lab3r6s14.42abudhabi.ae>
2023-07-19 12:15:11 +0400

    Solve an issue in checker.c about ft_putstr

--- src/checker.c
+++ src/checker.c
@@ -6,7 +6,7 @@
 /*   By: aajaanan <aajaanan@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 16:31:13 by aajaanan          #+#    #+#             */
-/*   Updated: 2023/07/19 12:06:29 by aajaanan         ###   ########.fr       */
+/*   Updated: 2023/07/19 12:14:26 by aajaanan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,7 @@ int main(int argc, char *argv[])
 
 	a = get_validated_stack(argc, argv);
 	if (a == NULL)
-		ft_putstr("Error\n", 2);
+		ft_putstr_fd("Error\n", 2);
 	else
 	{
 		b = (t_stack *)malloc(sizeof(t_stack));
